0be13bdb93aefdbafd628e385b00f5f7323ffb15
ADavlatov <enderbro306@gmailc.com>
2024-09-09 22:47:45 +0300

    Add validation service class

---
+++ Yosan.Auth/Services/ValidationService.cs
@@ -0,0 +1,19 @@
+namespace Yosan.Auth.Services;
+
+public class ValidationService
+{
+    public async Task ValidateLogInRequest()
+    {
+        
+    }
+    
+    public async Task ValidateSignInRequest()
+    {
+        
+    }
+    
+    public async Task ValidateToken()
+    {
+        
+    }
+}
