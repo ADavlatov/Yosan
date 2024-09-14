9668eb889cdcfaa1f9ee8a42c7e89bf86e5731a8
ADavlatov <a@davlatov.space>
2024-09-15 00:44:40 +0300

    Add log in method

--- Yosan.Auth/Services/ProtobufMethods/LogIn.cs
+++ Yosan.Auth/Services/ProtobufMethods/LogIn.cs
@@ -1,5 +1,7 @@
 
+using System.IdentityModel.Tokens.Jwt;
 using Yosan.Auth.Contexts;
+using Yosan.Auth.Services.ValidationMethods;
 
 namespace Yosan.Auth.Services.ProtobufMethods
 {
@@ -7,10 +9,31 @@ namespace Yosan.Auth.Services.ProtobufMethods
     {
         public async Task<LogInResponse> Authorize(LogInRequest request, UserContext db)
         {
-            var accessToken = new TokenService().GetJwtToken(request.Username, 60000);
-            var refreshToken = new TokenService().GetJwtToken(request.Username, 600000);
-            
-            return new LogInResponse{ };
+            LogInValidator logInValidator = new LogInValidator(db);
+            var validationResult = await logInValidator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+            {
+                return new LogInResponse
+                {
+                    IsSucceed = false,
+                    UsernameErrors = string.Join(",",
+                        validationResult.Errors.Where(x => x.ErrorMessage.Contains("username"))),
+                    PasswordErrors = string.Join(",",
+                        validationResult.Errors.Where(x => x.ErrorMessage.Contains("password")))
+                };
+            }
+
+            return new LogInResponse
+            {
+                IsSucceed = true,
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(TokenService.GetJwtToken(request.Username, 1)),
+                RefreshToken = new JwtSecurityTokenHandler().WriteToken(TokenService.GetJwtToken(request.Username, 4320)),
+                UserId = db.Users.FirstOrDefault(x =>
+                        (x.Username == request.Username || x.Email == request.Username) && x.Password == request.Password)!
+                    .Id
+                    .ToString()
+            };
         }
     }
 }
