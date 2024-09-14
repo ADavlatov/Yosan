cba37a5ac4a37a1e9dd4cf7319079570d605d669
ADavlatov <a@davlatov.space>
2024-09-15 00:42:27 +0300

    Add validators

---
+++ Yosan.Auth/Services/ValidationMethods/LogInValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Yosan.Auth.Contexts;
+
+namespace Yosan.Auth.Services.ValidationMethods;
+
+public class LogInValidator : AbstractValidator<LogInRequest>
+{
+    public LogInValidator(UserContext db)
+    {
+        RuleFor(x => x)
+            .Must(x => db.Users.FirstOrDefault(y =>
+                (y.Username == x.Username || y.Email == x.Username) && y.Password == x.Password) != null)
+            .WithMessage("Invalid username or password");
+        RuleFor(x => x.Username).MinimumLength(4)
+            .WithMessage("The username or email must consist of at least 4 characters").MaximumLength(30).NotEmpty()
+            .WithMessage("Enter your username or email");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Enter the password");
+    }
+}
---
+++ Yosan.Auth/Services/ValidationMethods/RefreshTokenValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using FluentValidation;
+
+namespace Yosan.Auth.Services.ValidationMethods;
+
+public class RefreshTokenValidator : AbstractValidator<AccessTokenRequest>
+{
+    public RefreshTokenValidator()
+    {
+        JwtSecurityTokenHandler jwt = new JwtSecurityTokenHandler();
+        RuleFor(x => x.RefreshToken).Must(x => jwt.CanReadToken(x))
+            .WithMessage("The token cannot be read").NotEmpty().WithMessage("The field cannot be empty");
+    }
+}
