3155b2f2fa3c21664f5a2121caa142d4517ad0f6
ADavlatov <a@davlatov.space>
2024-09-15 00:44:01 +0300

    Minor changes

--- Yosan.Auth/Models/User.cs
+++ Yosan.Auth/Models/User.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Yosan.Auth.Models;
 
-namespace Yosan.Auth.Models
+public class User
 {
-    public class User
-    {
-        public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
--- Yosan.Auth/Services/TokenService.cs
+++ Yosan.Auth/Services/TokenService.cs
@@ -7,22 +7,25 @@ namespace Yosan.Auth.Services;
 
 public class TokenService
 {
-    public JwtSecurityToken GetJwtToken(string username, int lifetime)
+    // Генерация JWT токена
+    public static JwtSecurityToken GetJwtToken(string username, int lifetime)
     {
         return new(issuer: AuthOptions.Issuer,
             audience: AuthOptions.Audience,
-            claims: GetClaims(username).Claims,
+            claims: GetClaims(username)
+                .Claims,
             expires: DateTime.UtcNow.Add(TimeSpan.FromDays(lifetime)),
             signingCredentials: new(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
     }
 
-    private ClaimsIdentity GetClaims(string username)
+    //Получение claims для генерации JWT токена
+    private static ClaimsIdentity GetClaims(string usernameOrEmail)
     {
         var claims = new List<Claim>
         {
-            new(ClaimTypes.Name, username)
+            new(ClaimTypes.Name, usernameOrEmail)
         };
-
+        
         ClaimsIdentity claimsIdentity =
             new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
                 ClaimsIdentity.DefaultRoleClaimType);
@@ -36,7 +39,7 @@ public class TokenService
 
         public const string Audience = "Yosan.Client";
 
-        private const string Key = "mysupersecret_secretkey!123";
+        const string Key = "mysupersecret_secretkey!123";
 
         public static SymmetricSecurityKey GetSymmetricSecurityKey() => new(Encoding.UTF8.GetBytes(Key));
     }
