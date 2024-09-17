a49c222544683fe85d2763927affb1df37ccd6df
ADavlatov <enderbro306@gmailc.com>
2024-09-17 10:52:18 +0300

    Add protobuf methods

--- Yosan.Gateway/Services/RouterService.cs
+++ Yosan.Gateway/Services/RouterService.cs
@@ -5,31 +5,25 @@ namespace Yosan.Gateway.Services;
 public class RouterService
 {
     private readonly Auth.AuthClient _authClient;
+
     public RouterService()
     {
-        var channel = GrpcChannel.ForAddress("");
+        var channel = GrpcChannel.ForAddress("https://localhost:7231");
         _authClient = new Auth.AuthClient(channel);
     }
-    public async Task Execute(WebApplication app)
-    {
-        app.MapPost("/api/v1/auth/signIn", (SignInRequest request) =>
-        {
-            
-        });
 
-        app.MapPost("api/v1/auth/logIn", (LogInRequest request) =>
-        {
-            
-        });
-        
-        app.MapPost("api/v1/auth/validate", (TokenValidationRequest request) =>
-        {
-            
-        });
-        
-        app.MapPost("api/v1/auth/refresh", (RefreshTokenRequest request) =>
-        {
-            
-        });
+    public void Execute(WebApplication app)
+    {
+        app.MapPost("/api/v1/auth/signIn",
+            async (SignInRequest request) => await _authClient.SignInUserAsync(request));
+
+        app.MapPost("api/v1/auth/logIn",
+            async (LogInRequest request) => await _authClient.LogInUserAsync(request));
+
+        app.MapPost("api/v1/auth/validate",
+            async (TokenValidationRequest request) => await _authClient.ValidateTokenAsync(request));
+
+        app.MapPost("api/v1/auth/refresh",
+            async (RefreshTokenRequest request) => await _authClient.GetAccessTokenAsync(request));
     }
 }
