09f5669e302b0dbe796c253070f422bae5bf4634
ADavlatov <enderbro306@gmailc.com>
2024-09-17 10:31:01 +0300

    Add endpoints

--- Yosan.Gateway/Services/RouterService.cs
+++ Yosan.Gateway/Services/RouterService.cs
@@ -1,20 +1,33 @@
+using Grpc.Net.Client;
+
 namespace Yosan.Gateway.Services;
 
-public class RouterService(WebApplication app)
+public class RouterService
 {
-    public async Task Execute()
+    private readonly Auth.AuthClient _authClient;
+    public RouterService()
     {
-        app.MapPost("/api/v1/auth/signIn", () =>
+        var channel = GrpcChannel.ForAddress("");
+        _authClient = new Auth.AuthClient(channel);
+    }
+    public async Task Execute(WebApplication app)
+    {
+        app.MapPost("/api/v1/auth/signIn", (SignInRequest request) =>
         {
-
+            
         });
 
-        app.MapPost("api/v1/auth/logIn", () =>
+        app.MapPost("api/v1/auth/logIn", (LogInRequest request) =>
         {
             
         });
         
-        app.MapPost("api/v1/auth/logIn", () =>
+        app.MapPost("api/v1/auth/validate", (TokenValidationRequest request) =>
+        {
+            
+        });
+        
+        app.MapPost("api/v1/auth/refresh", (RefreshTokenRequest request) =>
         {
             
         });
