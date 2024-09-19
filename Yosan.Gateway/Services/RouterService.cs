78cc1e390a55fa1614542d9b5adb1a189da7ebf6
ADavlatov <a@davlatov.space>
2024-09-19 10:26:58 +0300

    Add request validation in gateway

--- Yosan.Gateway/Services/RouterService.cs
+++ Yosan.Gateway/Services/RouterService.cs
@@ -1,4 +1,5 @@
 using Grpc.Net.Client;
+using Yosan.Gateway.Services.Validation;
 
 namespace Yosan.Gateway.Services;
 
@@ -15,15 +16,56 @@ public class RouterService
     public void Execute(WebApplication app)
     {
         app.MapPost("/api/v1/auth/signIn",
-            async (SignInRequest request) => await _authClient.SignInUserAsync(request));
+            async (SignInRequest request) =>
+            {
+                var result = await new SignInValidator().ValidateAsync(request);
+
+                if (!result.IsValid)
+                {
+                    return new SignInResponse { IsSucceed = false, Errors = string.Join(", ", result.Errors) };
+                }
+
+                return await _authClient.SignInUserAsync(request);
+            });
+
 
         app.MapPost("api/v1/auth/logIn",
-            async (LogInRequest request) => await _authClient.LogInUserAsync(request));
+            async (LogInRequest request) =>
+            {
+                var result = await new LogInValidator().ValidateAsync(request);
+
+                if (!result.IsValid)
+                {
+                    return new LogInResponse { IsSucceed = false, Errors = string.Join(", ", result.Errors) };
+                }
+
+                return await _authClient.LogInUserAsync(request);
+            });
 
         app.MapPost("api/v1/auth/validate",
-            async (TokenValidationRequest request) => await _authClient.ValidateTokenAsync(request));
+            async (TokenValidationRequest request) =>
+            {
+                var result = await new AccessTokenValidator().ValidateAsync(request);
+
+                if (!result.IsValid)
+                {
+                    return new TokenValidationResponse { IsSucceed = false, Errors = string.Join(", ", result.Errors) };
+                }
+
+                return await _authClient.ValidateTokenAsync(request);
+            });
 
         app.MapPost("api/v1/auth/refresh",
-            async (RefreshTokenRequest request) => await _authClient.GetAccessTokenAsync(request));
+            async (RefreshTokenRequest request) =>
+            {
+                var result = await new RefreshTokenValidator().ValidateAsync(request);
+
+                if (!result.IsValid)
+                {
+                    return new RefreshTokenResponse { IsSucceed = false, Errors = string.Join(", ", result.Errors) };
+                }
+
+                return await _authClient.GetAccessTokenAsync(request);
+            });
     }
 }
