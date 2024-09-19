7eedc9d9c95619b67ac1938fe02a5107a0a41557
ADavlatov <a@davlatov.space>
2024-09-19 12:21:27 +0300

    Add services

---
+++ Yosan.Core/Services/IncomesService.cs
@@ -0,0 +1,16 @@
+using Yosan.Core.Interfaces;
+
+namespace Yosan.Core.Services;
+
+public class IncomesService : IService
+{
+    public async Task Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Yosan.Core/Services/SavingsService.cs
@@ -0,0 +1,16 @@
+using Yosan.Core.Interfaces;
+
+namespace Yosan.Core.Services;
+
+public class SavingsService : IService
+{
+    public async Task Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
