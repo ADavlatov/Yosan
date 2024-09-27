a52e78332cc0f0364e179a9e3a3611e577d7a313
ADavlatov <a@davlatov.space>
2024-09-27 16:53:07 +0300

    Update grpc methods and .proto

--- Yosan.Core/Services/CoreService.cs
+++ Yosan.Core/Services/CoreService.cs
@@ -5,33 +5,8 @@ namespace Yosan.Core.Services;
 
 public class CoreService(CoreContext db) : Core.CoreBase
 {
-    public override Task<AddIncomeResponse> AddIncome(AddIncomeRequest request, ServerCallContext context)
+    public override Task<AddCategoryResponse> AddCategory(AddCategoryRequest request, ServerCallContext context)
     {
-        return base.AddIncome(request, context);
-    }
-
-    public override Task<DeleteIncomeResponse> DeleteIncome(DeleteIncomeRequest request, ServerCallContext context)
-    {
-        return base.DeleteIncome(request, context);
-    }
-
-    public override Task<AddExpenseResponse> AddExpense(AddExpenseRequest request, ServerCallContext context)
-    {
-        return base.AddExpense(request, context);
-    }
-
-    public override Task<DeleteExpenseResponse> DeleteExpense(DeleteExpenseRequest request, ServerCallContext context)
-    {
-        return base.DeleteExpense(request, context);
-    }
-
-    public override Task<AddSavingResponse> AddSaving(AddSavingRequest request, ServerCallContext context)
-    {
-        return base.AddSaving(request, context);
-    }
-
-    public override Task<DeleteSavingResponse> DeleteSaving(DeleteSavingRequest request, ServerCallContext context)
-    {
-        return base.DeleteSaving(request, context);
+        return base.AddCategory(request, context);
     }
 }
