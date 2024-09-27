e913dee6c4a1560f81958726b6b8370dc5813c56
ADavlatov <a@davlatov.space>
2024-09-28 01:58:10 +0300

    Change type

--- Yosan.Core/Models/Category.cs
+++ Yosan.Core/Models/Category.cs
@@ -1,12 +1,10 @@
-﻿using Yosan.Core.Enums;
-
-namespace Yosan.Core.Models;
+﻿namespace Yosan.Core.Models;
 
 public class Category
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public CategoryTypes Type { get; set; }
+    public int Type { get; set; }
     public string UserId { get; set; }
     public List<CategoryUnit> Units { get; set; }
 }
