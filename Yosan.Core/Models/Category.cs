caafedf77085985172320b515b0d0da6dda0245d
ADavlatov <a@davlatov.space>
2024-09-28 14:57:35 +0300

    Update category model

--- Yosan.Core/Models/Category.cs
+++ Yosan.Core/Models/Category.cs
@@ -5,6 +5,7 @@ public class Category
     public Guid Id { get; set; }
     public string Name { get; set; }
     public int Type { get; set; }
+    public float Sum { get; set; }
     public string UserId { get; set; }
     public List<CategoryUnit> Units { get; set; }
 }
