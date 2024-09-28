74e76bfb1c2361c5ff0940438d2cf97a248b21c7
ADavlatov <a@davlatov.space>
2024-09-28 14:58:14 +0300

    Update category service

--- Yosan.Core/Services/CategoryService.cs
+++ Yosan.Core/Services/CategoryService.cs
@@ -7,7 +7,7 @@ namespace Yosan.Core.Services;
 
 public class CategoryService(CoreContext db)
 {
-    public async Task<AddCategoryResponse> AddCategory(AddCategoryRequest request)
+    public async Task<AddCategoryResponse> Add(AddCategoryRequest request)
     {
         var category =
             await db.Categories.FirstOrDefaultAsync(x => x.UserId == request.UserId && x.Name == request.Name);
@@ -27,7 +27,7 @@ public class CategoryService(CoreContext db)
         return new AddCategoryResponse { IsSucceed = true, Status = 200, Error = "" };
     }
 
-    public async Task<GetCategoriesResponse> GetCategories(GetCategoriesRequest request)
+    public async Task<GetCategoriesResponse> Get(GetCategoriesRequest request)
     {
         RepeatedField<CategoryObject> categoriesRepeatedField = new RepeatedField<CategoryObject>();
         RepeatedField<UnitObject> unitsRepeatedField = new RepeatedField<UnitObject>();
@@ -37,6 +37,7 @@ public class CategoryService(CoreContext db)
         {
             return new GetCategoriesResponse { IsSucceed = false, Status = 400, Error = "UserId failed" };
         }
+
         //@TODO переписать это непотребство
         foreach (var category in categories)
         {
@@ -48,26 +49,50 @@ public class CategoryService(CoreContext db)
 
             categoriesRepeatedField.Add(new CategoryObject
             {
+                Id = category.Id.ToString(),
                 Name = category.Name, UserId = category.UserId, Type = category.Type, Units = { unitsRepeatedField }
             });
             unitsRepeatedField.Clear();
         }
 
-        return new GetCategoriesResponse { IsSucceed = true, Status = 200, Error = "", Categories = { categoriesRepeatedField }};
+        return new GetCategoriesResponse
+            { IsSucceed = true, Status = 200, Error = "", Categories = { categoriesRepeatedField } };
     }
 
-    public async Task<RemoveCategoryResponse> RemoveCategory(RemoveCategoryRequest request)
+    public async Task<RemoveCategoryResponse> Remove(RemoveCategoryRequest request)
     {
-        var category = await db.Categories.FirstOrDefaultAsync(x => x.UserId == request.UserId && x.Id.ToString() == request.Id);
+        var category =
+            await db.Categories.FirstOrDefaultAsync(x => x.UserId == request.UserId && x.Id.ToString() == request.Id);
 
         if (category == null)
         {
-            return new RemoveCategoryResponse{ IsSucceed = false, Status = 400, Error = "Category not found" };
+            return new RemoveCategoryResponse { IsSucceed = false, Status = 400, Error = "Category not found" };
         }
 
         db.Categories.Remove(category);
         await db.SaveChangesAsync();
-        
-        return new RemoveCategoryResponse{ IsSucceed = true, Status = 200, Error = "" };
+
+        return new RemoveCategoryResponse { IsSucceed = true, Status = 200, Error = "" };
+    }
+
+    public async Task<DepositCategoryResponse> Deposit(DepositCategoryRequest request)
+    {
+        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id.ToString() == request.CategoryId);
+
+        if (category == null)
+        {
+            return new DepositCategoryResponse { IsSucceed = false, Status = 400, Error = "Category not found" };
+        }
+
+        if (category.Type != 2)
+        {
+            return new DepositCategoryResponse { IsSucceed = false, Status = 400, Error = "Wrong category type" };
+        }
+
+        category.Sum += float.Parse(request.Sum);
+        db.Categories.Update(category);
+        await db.SaveChangesAsync();
+
+        return new DepositCategoryResponse { IsSucceed = true, Status = 200, Error = "" };
     }
 }
