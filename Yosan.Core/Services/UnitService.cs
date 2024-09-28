68cf429bb7e1abcac73132ccc9fb330a4d0f6843
ADavlatov <a@davlatov.space>
2024-09-28 14:58:02 +0300

    Add unit service

---
+++ Yosan.Core/Services/UnitService.cs
@@ -0,0 +1,48 @@
+﻿using Google.Protobuf.Collections;
+using Microsoft.EntityFrameworkCore;
+using Yosan.Core.Contexts;
+using Yosan.Core.Models;
+
+namespace Yosan.Core.Services;
+
+public class UnitService(CoreContext db)
+{
+    public async Task<AddUnitResponse> Add(AddUnitRequest request)
+    {
+        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id.ToString() == request.CategoryId);
+
+        if (category == null)
+        {
+            return new AddUnitResponse { IsSucceed = false, Status = 400, Error = "Category not found" };
+        }
+
+        await db.CategoryUnits.AddAsync(new CategoryUnit
+        {
+            UserId = request.UserId, Name = request.Name, Sum = float.Parse(request.Sum),
+            Date = DateOnly.Parse(request.Date), Category = category
+        });
+        await db.SaveChangesAsync();
+
+        return new AddUnitResponse { IsSucceed = true, Status = 200, Error = "" };
+    }
+
+    public async Task<GetUnitsResponse> Get(GetUnitsRequest request)
+    {
+        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id.ToString() == request.CategoryId);
+
+        if (category == null)
+        {
+            return new GetUnitsResponse { IsSucceed = false, Status = 400, Error = "Category not found" };
+        }
+
+        RepeatedField<UnitObject> units = new RepeatedField<UnitObject>();
+
+        foreach (var unit in db.CategoryUnits.Where(x => x.Category == category))
+        {
+            units.Add(new UnitObject
+                { Id = unit.Id.ToString(), Name = unit.Name, Sum = unit.Sum.ToString(), Date = unit.Date.ToString() });
+        }
+        
+        return new GetUnitsResponse{ IsSucceed = true, Status = 200, Error = "", Units = { units }};
+    }
+}
