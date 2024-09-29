bfee49960606f47862ed54b5dc30968e41b1512b
ADavlatov <a@davlatov.space>
2024-09-29 22:36:23 +0300

    Add core methods

--- Yosan.Core/Services/CoreService.cs
+++ Yosan.Core/Services/CoreService.cs
@@ -5,8 +5,33 @@ namespace Yosan.Core.Services;
 
 public class CoreService(CoreContext db) : Core.CoreBase
 {
-    public override Task<AddCategoryResponse> AddCategory(AddCategoryRequest request, ServerCallContext context)
+    public override async Task<AddCategoryResponse> AddCategory(AddCategoryRequest request, ServerCallContext context)
     {
-        return base.AddCategory(request, context);
+        return await new CategoryService(db).Add(request);
+    }
+
+    public override async Task<GetCategoriesResponse> GetCategories(GetCategoriesRequest request, ServerCallContext context)
+    {
+        return await new CategoryService(db).Get(request);
+    }
+
+    public override async Task<DepositCategoryResponse> DepositCategory(DepositCategoryRequest request, ServerCallContext context)
+    {
+        return await new CategoryService(db).Deposit(request);
+    }
+
+    public override async Task<RemoveCategoryResponse> RemoveCategory(RemoveCategoryRequest request, ServerCallContext context)
+    {
+        return await new CategoryService(db).Remove(request);
+    }
+
+    public override async Task<AddUnitResponse> AddUnit(AddUnitRequest request, ServerCallContext context)
+    {
+        return await new UnitService(db).Add(request);
+    }
+
+    public override async Task<GetUnitsResponse> GetUnits(GetUnitsRequest request, ServerCallContext context)
+    {
+        return await new UnitService(db).Get(request);
     }
 }
