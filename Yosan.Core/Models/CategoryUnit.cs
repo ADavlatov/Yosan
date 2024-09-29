d8e81533d133f49700453ab5694ebb880839a837
ADavlatov <a@davlatov.space>
2024-09-29 22:35:19 +0300

    Add categoryId and delete userId in unit

--- Yosan.Core/Models/CategoryUnit.cs
+++ Yosan.Core/Models/CategoryUnit.cs
@@ -6,6 +6,6 @@ public class CategoryUnit
     public string Name { get; set; }
     public float Sum { get; set; }
     public DateOnly Date { get; set; }
-    public string UserId { get; set; }
+    public Guid CategoryId { get; set; }
     public Category Category { get; set; }
 }
