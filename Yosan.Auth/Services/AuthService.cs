080257c6c858b2b3d82f8b2418ce4ef69972e19a
ADavlatov <a@davlatov.space>
2024-10-01 10:37:33 +0300

    Remove check user method

--- Yosan.Auth/Services/AuthService.cs
+++ Yosan.Auth/Services/AuthService.cs
@@ -20,23 +20,11 @@ public class AuthService(UserContext db) : Auth.AuthBase
     public override async Task<TokenValidationResponse> ValidateToken(TokenValidationRequest request,
         ServerCallContext context)
     {
-        return await new Token().Validate(request);
+        return await new Token(db).Validate(request);
     }
 
     public override async Task<RefreshTokenResponse> GetAccessToken(RefreshTokenRequest request, ServerCallContext context)
     {
-        return await new Token().Get(request, db);
-    }
-
-    public override async Task<CheckUserResponse> CheckUser(CheckUserRequest request, ServerCallContext context)
-    {
-        var user = await db.Users.FirstOrDefaultAsync(x => x.Id.ToString() == request.UserId);
-
-        if (user == null)
-        {
-            return new CheckUserResponse { IsSucceed = false, Status = 400, Error = "User does not exist" };
-        }
-        
-        return new CheckUserResponse { IsSucceed = true, Status = 200, Error = "" };
+        return await new Token(db).Get(request, db);
     }
 }
