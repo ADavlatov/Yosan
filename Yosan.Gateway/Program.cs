b6ab51a6c59aa10c3ad61fbdd9c15ba5bf2ebeb6
ADavlatov <a@davlatov.space>
2024-10-03 10:47:28 +0300

    Add hosted service with rabbit listener

--- Yosan.Gateway/Program.cs
+++ Yosan.Gateway/Program.cs
@@ -1,6 +1,10 @@
+using Yosan.Gateway.RabbitMq;
 using Yosan.Gateway.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHostedService<RabbitMqListener>();
+
 var app = builder.Build();
 
 new RouterService().Execute(app);
