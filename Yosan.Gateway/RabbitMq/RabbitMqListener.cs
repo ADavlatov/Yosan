ebcc2499ea9128e883d72c37faa596c85153d691
ADavlatov <a@davlatov.space>
2024-10-09 16:07:15 +0300

    Add rabbitMQ listener

--- Yosan.Gateway/RabbitMq/RabbitMqListener.cs
+++ Yosan.Gateway/RabbitMq/RabbitMqListener.cs
@@ -1,7 +1,8 @@
-using System.Diagnostics;
 using System.Text;
+using Google.Protobuf;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using Yosan.Gateway.Helpers;
 using Yosan.Gateway.Services;
 
 namespace Yosan.Gateway.RabbitMq;
@@ -10,10 +11,12 @@ public class RabbitMqListener : BackgroundService
 {
     private IConnection _connection;
     private IModel _channel;
+    private RouterService _router;
 
     public RabbitMqListener(IConfiguration config)
     {
         //@TODO вынести подключение в asppsettings
+        _router = new RouterService();
         var factory = new ConnectionFactory { HostName = "localhost" };
         _connection = factory.CreateConnection();
         _channel = _connection.CreateModel();
@@ -25,12 +28,25 @@ public class RabbitMqListener : BackgroundService
         stoppingToken.ThrowIfCancellationRequested();
 
         var consumer = new EventingBasicConsumer(_channel);
-        consumer.Received += (ch, ea) =>
+        consumer.Received += async (ch, ea) =>
         {
             var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+            var path = CheckRequestPath.Check(content);
+            IBufferMessage? message = null;
+            if (path != "")
+            {
+               message = await _router.Execute(path, content);
+            }
 
-            Console.WriteLine($"Запрос: {content}");
+            if (message != null)
+            {
+                var props = _channel.CreateBasicProperties();
+                props.CorrelationId = ea.BasicProperties.CorrelationId;
 
+                _channel.BasicPublish(exchange: "", routingKey: ea.BasicProperties.ReplyTo, basicProperties: props,
+                    body: Encoding.UTF8.GetBytes(message.ToString()));
+            }
+            
             _channel.BasicAck(ea.DeliveryTag, false);
         };
         _channel.BasicConsume("MyQueue", false, consumer);
